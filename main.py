d89b99f299405581ab2a20655b9779b04c61c847
szer02 <santiago.shego@gmail.com>
2023-10-25 19:47:06 -0300

    Correções Feitas no CRUD, arquivo main contém detalhamento da att

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 from router import curso_router
 
 app = FastAPI(
-    title = "Prova de POO. Discente: Santiago da Rocha Souza",
-    description  = "Criação de CRUD usando o modelo CURSO"
+    title = "Tarefa de POO/Prova com atualizações. Discente: Santiago da Rocha Souza",
+    description  = "Criação de CRUD usando o modelo CURSO - Foi feito a correção onde ao atualizar, listar por id e deletar, os mesmo puxavam por índice e não pelo próprio id. Sendo assim ao inserir o código_id = 1 não retornava nada pois era a posição 0, colocando no curso_id = 0, retornava o curso_id = 1, não seria algo que deixaria de funcionar, mas deixaria o código com erro de lógica.",
+    version = "0.1.1"
+
 )
 
 app.include_router(curso_router.router, prefix = "/Cursos")
--- router/curso_router.py
+++ router/curso_router.py
@@ -12,12 +12,18 @@ def listar_cursos():
     return cursos
 
 #Método para listar curso por ID
-'''@router.get("/{curso_id}", response_model = CursoModel)
-def listar_cursos_id(curso_id: int):
-    if curso_id >= len(cursos):
-        raise HTTPException(status_code= 404, detail="Curso não encontrado")
-    curso_encontrado = cursos[curso_id]
-    return curso_encontrado '''
+@router.get("/{curso_id}", response_model=CursoModel)
+def listar_curso_por_id(curso_id: int):
+    curso_encontrado = None
+    for curso in cursos:
+        if curso.id == curso_id:
+            curso_encontrado = curso
+            break
+
+    if curso_encontrado is None:
+        raise HTTPException(status_code=404, detail="Curso não encontrado")
+
+    return curso_encontrado
 
 #Método onde cadastra o curso:
 @router.post("/cursos/", response_model= CursoModel)
@@ -26,16 +32,32 @@ def cadastrar_cursos(curso: CursoModel):
     return curso
 
 #Método onde atualiza um curso:
-@router.put("/cursos/{curso_id}", response_model= CursoModel)
-def atualizar_cursos(curso_id: int, updated_curso:  CursoModel):
-    if curso_id >= len(cursos):
-        raise HTTPException(status_code= 404, detail="Curso não encontrado")
-    cursos[curso_id] = updated_curso
+@router.put("/cursos/{curso_id}", response_model=CursoModel)
+def atualizar_cursos(curso_id: int, updated_curso: CursoModel):
+    curso_encontrado = None
+    for i, curso in enumerate(cursos):  # "i" , vai servir para saber em qual indice (posição) o curso esta percorrendo, e o Curso vai verificar se o número atribuido ao atributo id é igual ao curso_id.
+        if curso.id == curso_id:  # Certifique-se de usar o atributo correto, como 'id' neste exemplo.
+            curso_encontrado = curso
+            cursos[i] = updated_curso
+            break
+
+    if curso_encontrado is None:
+        raise HTTPException(status_code=404, detail="Curso não encontrado")
+
     return updated_curso
 
-@router.delete("/cursos/{curso_id}", response_model = CursoModel)
+
+#Método onde deleta um curso:
+@router.delete("/cursos/{curso_id}", response_model=CursoModel)
 def deletar_cursos(curso_id: int):
-    if curso_id >= len(cursos):
-        raise HTTPException(status_code = 404, detail = "Curso não encontrad")
-    deleted_curso = cursos.pop(curso_id)
-    return deleted_curso
+    curso_encontrado = None
+    for i, curso in enumerate(cursos):
+        if curso.id == curso_id:
+            curso_encontrado = curso
+            break
+
+    if curso_encontrado is None:
+        raise HTTPException(status_code=404, detail="Curso não encontrado")
+
+    cursos.pop(i)  # Remove o curso encontrado na posição i, identificando o id através da variável curso
+    return curso_encontrado
